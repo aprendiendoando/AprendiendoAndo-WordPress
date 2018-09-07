801d51efc3375d12d994bc348de7b643b8f1454c
Yan Arlex Vallejo <yan.vallejo@kuntur.digital>
2018-09-07 17:49:37 -0500

    Clase #3 - Arreglando bugs del loop cursos en el home

---
+++ content-missing.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * The template part for displaying a message that posts cannot be found
+ */
+?>
+<article class="content-not-found">
+	<p><?php _e( ' <span> Oops </span> ¡No tenemos nada para mostrar :( ' , 'aprendiendoando' ); ?></p>
+</article>

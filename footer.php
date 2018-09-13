e53034b90b6e66c6804daa6ea37d320aff6d1d0a
Yan Arlex Vallejo <yan.vallejo@kuntur.digital>
2018-09-13 16:11:02 -0500

    Clase 5 - Terminando el Footer

--- footer.php
+++ footer.php
@@ -1,47 +1,49 @@
 <footer class="footer box">
         <figure class="sin-margin">
-            <a href="#">
-                <img src="<?php echo get_template_directory_uri() ?>/img/aprendiendoando.png" alt="AprendiendoAndo" width="223" height="">
+            <a href="<?php echo esc_url( home_url( '/' ) ); ?>">
+                <img src="<?php the_field('header_logo', 'option'); ?>" alt="<?php bloginfo('name'); ?>">
             </a>
         </figure>
         <div class="footer__mail">
             <i class="aprendiendoando-correo"></i>
-            <a class="decoration-none" href="#">yan@aprendiendoando.com</a>
+            <a class="decoration-none" href="mailto:<?php the_field('header_correo_principal', 'option'); ?>?subject=<?php bloginfo('name'); ?>">
+                <?php the_field('header_correo_principal', 'option'); ?>
+            </a>
         </div>
+        <?php
+            $header_redes_sociales = get_field('header_redes_sociales','option');
+        ?>
+        <?php
+            if($header_redes_sociales){
+        ?>
         <ul class="list-none d-flex">
             <li>
-                <a class="decoration-none" href="https://twitter.com/yavallejo" title="Twitter Yan Arlex Vallejo" target="_blank">
+                <a class="decoration-none" href="<?php echo $header_redes_sociales['twitter_url']; ?>" title="Twitter <?php bloginfo('name'); ?>" target="_blank">
                     <i class="aprendiendoando-twitter"></i>
                 </a>
             </li>
             <li>
-                <a class="decoration-none" href="https://facebook.com/aprendiendoando" title="Facebook AprendiendoAndo" target="_blank">
+                <a class="decoration-none" href="<?php echo $header_redes_sociales['facebook_url']; ?>" title="Facebook <?php bloginfo('name'); ?>" target="_blank">
                     <i class="aprendiendoando-facebook"></i>
                 </a>
             </li>
             <li>
-                <a class="decoration-none" href="https://www.youtube.com/aprendiendoando" title="Youtube Aprendiendoando" target="_blank">
+                <a class="decoration-none" href="<?php echo $header_redes_sociales['youtube_url']; ?>" title="Youtube <?php bloginfo('name'); ?>" target="_blank">
                     <i class="aprendiendoando-youtube"></i>
                 </a>
             </li>
             <li>
-                <a class="decoration-none" href="http://www.aprendiendoando.com/" title="RSS Aprendiendoando" target="_blank">
+                <a class="decoration-none" href="<?php echo $header_redes_sociales['rss_url']; ?>" title="RSS <?php bloginfo('name'); ?>" target="_blank">
                     <i class="aprendiendoando-rss"></i>
                 </a>
             </li>
         </ul>
+        <?php
+            }
+        ?>
     </footer> <!-- end Footer -->
     <?php wp_footer(); ?>
     <!-- Scripts -->
-
-
-    <!-- <script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
-    <script>
-        window.jQuery || document.write('<script src="js/vendor/jquery-1.12.0.min.js"><\/script>')
-    </script>
-    <script src="js/plugins.js"></script>
-    <script src="js/main.js"></script> -->
-
     <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
     <script>
             (function (b, o, i, l, e, r) {

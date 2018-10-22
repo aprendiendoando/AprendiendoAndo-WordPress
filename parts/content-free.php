34261569e308c78e7b4cc771f313eed22136b3ef
Yan Arlex Vallejo <yavallejor@gmail.com>
2018-10-22 12:31:06 -0500

    Fixed enlace externo image

--- parts/content-free.php
+++ parts/content-free.php
@@ -1,20 +1,26 @@
 <article class="list-courses__item">
     <figure class="list-courses__image sin-margin">
-        <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
-            <?php
-                if ( has_post_thumbnail() ) :
-                    the_post_thumbnail('full', ['class' => 'img-responsive']);
-                else:
-            ?>
-                 <img src="<?php bloginfo('template_directory') ?>/img/aprendiendoando-bg.jpg" alt="<?php the_title(); ?>" /></a>
-            <?php
-                endif;
-            ?>
-        </a>        
-    </figure>
+        <?php
+            $link = get_field('enlace');
+            if( $link ):
+        ?>
+            <a href="<?php echo $link;?>" title="<?php the_title(); ?>">
+                <?php
+                    if ( has_post_thumbnail() ) :
+                        the_post_thumbnail('full', ['class' => 'img-responsive']);
+                    else:
+                ?>
+                    <img src="<?php bloginfo('template_directory') ?>/img/aprendiendoando-bg.jpg" alt="<?php the_title(); ?>" /></a>
+                <?php
+                    endif;
+                ?>
+            </a>  
+        <?php
+            endif;
+        ?>
+     </figure>
     <div class="list-courses__information">
         <?php 
-            $link = get_field('enlace');
             if( $link ): ?>
                 <a href="<?php echo $link;?>" target="_blank" class="decoration-none" title="<?php the_title(); ?>">
                     <h3 class="sin-margin"><?php the_title(); ?></h3>
@@ -24,7 +30,6 @@
     </div>
     <footer class="list-courses__author">
         <?php
-            $link = get_field('enlace');
             if( $link ): ?>
                 <a href="<?php echo $link;?>" class="button button--main" id="">Ver contenido</a>
             <?php endif; ?>
